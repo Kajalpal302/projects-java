ad83f76c7f4536f12ee572eb4752e0643f3be630
kajal <kajalpal6005@gmail.com>
2025-07-06 00:40:16 +0530

    init commit

---
+++ src/com/kajalpal/PhoneNo.java
@@ -0,0 +1,52 @@
+package com.kajalpal;
+
+import java.util.ArrayList;
+
+public class PhoneNo {
+    public static void main(String[] args) {
+        pad("", "12");
+        System.out.println(padRe("", "12").size());
+        System.out.println(padCount("","12"));
+    }
+
+    static void pad(String p, String up) {
+        if (up.isEmpty()) {
+            System.out.println(p);
+            return;
+        }
+        int digit = up.charAt(0) - '0';//this will convert the code to alphabet
+        for (int i = (digit - 1) * 3; i < digit * 3; i++) {
+            char ch = (char) ('a' + i);
+            pad(p + ch, up.substring(1));
+        }
+    }
+
+    static ArrayList<String> padRe(String p, String up) {
+        if (up.isEmpty()) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+
+        int digit = up.charAt(0) - '0';//this will convert the code to alphabet
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = (digit - 1) * 3; i < digit * 3; i++) {
+            char ch = (char) ('a' + i);
+            list.addAll(padRe(p + ch, up.substring(1)));
+        }
+        return list;
+    }
+
+    static int padCount(String p, String up) {
+        if (up.isEmpty()) {
+            return 1;
+        }
+        int count = 0;
+        int digit = up.charAt(0) - '0';//this will convert the code to alphabet
+        for (int i = (digit - 1) * 3; i < digit * 3; i++) {
+            char ch = (char) ('a' + i);
+            count = count + padCount(p + ch, up.substring(1));
+        }
+        return count;
+    }
+}
